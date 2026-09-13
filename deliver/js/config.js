a1b6e567dd254d3db1a33de57c2ad2f05fac0652
Clint <boztiktech@gmail.com>
2026-09-13 11:38:51 +0200

    commit

--- deliver/js/config.js
+++ deliver/js/config.js
@@ -1,10 +1,8 @@
 export const config = Object.freeze({
-  supabaseUrl: "https://hwcxxotgtqchcriascti.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3Y3h4b3RndHFjaGNyaWFzY3RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NTI5MzMsImV4cCI6MjEwMTUyODkzM30.bLyXIjvw0NcZQsyStPvq6d7nwDrMSycsTKuJkOtd9wU",
-  storageBucket: "deliveries",
-  publicBaseUrl: "https://www.boztik.com/deliver/",
+  apiBaseUrl: "https://deliver-api.boztik.com",
+  publicBaseUrl: "https://www.boztik.com/deliver-v2/",
   defaultExpiryHours: 24,
   maxUploadBytes: 250 * 1024 * 1024,
   allowedExtensions: ["zip", "jpg", "jpeg", "png", "psd", "tif", "tiff", "webp", "pdf", "ai", "eps"],
-  paypalUrl: "https://paypal.me/angry5p1c3"
+  paypalUrl: "http://paypal.me/angry5p1c3"
 });
